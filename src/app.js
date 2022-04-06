5acd935c31c2dc5c756e51987d22e892e1d3643b
minathedebugger <minagaid68@gmail.com>
2022-04-06 20:32:32 +0200

    adding Cors-Support

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 const axios = require('axios');
+const cors = require('cors')
+
 app.use(express.json()) 
+app.use(cors())
+
 app.get('/test', function (req, res) {
   res.send('Hello World!')
 });
@@ -132,3 +136,41 @@ app.post('/event', async function (req, res) {
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
 });
+
+async function createStreamJava(streamName, schema) {  
+  try {  
+  const data = `
+  {
+  "streamName": "${streamName}",
+  "schema": ${schema}
+  }`
+  const headers = {
+  'Content-Type': 'application/json'}
+  const response = await axios.post('http://localhost:4000/createstream', data, {
+  headers: headers
+  })
+  alert("it worked " + JSON.stringify(response.body))
+  } catch (error) {
+  alert(error)
+  console.error(error)
+  }
+  }
+  
+  async function schema(queryString) {  
+    try {  
+    const data = `
+    {
+      "queryString" : ${queryString}
+    }`
+    const headers = {
+    'Content-Type': 'application/json'}
+    const response = await axios.post('http://localhost:4000/schema', data, {
+    headers: headers
+    })
+    alert("it worked " + JSON.stringify(response.body))
+    } catch (error) {
+    alert(error)
+    console.error(error)
+    }
+    }
+  
